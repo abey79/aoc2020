7a3d8bdb18d89bc6fbb027c0f609254cdb840d55
Antoine Beyeler <abeyeler@ab-ware.com>
2020-12-11 22:51:34 +0100

    Day 11: numba optimisation

--- aoc2020/day11.py
+++ aoc2020/day11.py
@@ -1,5 +1,7 @@
 import aocd
+import numba
 import numpy as np
+from numba import njit
 
 TEST_DATA = """L.LL.LL.LL
 LLLLLLL.LL
@@ -59,35 +61,6 @@ def test_day11_part1():
     assert day11_part1(parse(TEST_DATA)) == 37
 
 
-def neighborhood_part2(data: np.ndarray) -> np.ndarray:
-    def first_occupied(line: np.ndarray) -> bool:
-        (idx,) = np.where(line == OCCUPIED)
-        if len(idx) == 0:
-            return False
-        (idx2,) = np.where(line == SEAT)
-        if len(idx2) == 0:
-            return True
-        return idx[0] < idx2[0]
-
-    data = np.pad(data, ((1, 1), (1, 1)), "constant")
-    out = np.empty(shape=data.shape, dtype=int)
-    for i in range(1, data.shape[0] - 1):
-        for j in range(1, data.shape[1] - 1):
-            out[i, j] = sum(
-                [
-                    first_occupied(data[i + 1 :, j]),
-                    first_occupied(data[i, j + 1 :]),
-                    first_occupied(data[i - 1 :: -1, j]),
-                    first_occupied(data[i, j - 1 :: -1]),
-                    first_occupied(np.diag(data[i + 1 :, j + 1 :])),
-                    first_occupied(np.diag(data[i - 1 :: -1, j - 1 :: -1])),
-                    first_occupied(np.diag(data[i - 1 :: -1, j + 1 :])),
-                    first_occupied(np.diag(data[i + 1 :, j - 1 :: -1])),
-                ]
-            )
-    return out[1:-1, 1:-1]
-
-
 def to_string(data: np.ndarray) -> str:
     return "\n".join(
         "".join("#" if c == OCCUPIED else ("L" if c == SEAT else ".") for c in data_line)
@@ -95,6 +68,36 @@ def to_string(data: np.ndarray) -> str:
     )
 
 
+@njit
+def first_occupied(line: np.ndarray) -> int:
+    (idx,) = np.where(line != EMPTY)
+    if len(idx) == 0:
+        return 0
+    else:
+        return 1 if line[idx[0]] == OCCUPIED else 0
+
+
+@njit
+def neighborhood_part2(data: np.ndarray) -> np.ndarray:
+    padded_data = np.zeros(shape=(data.shape[0] + 2, data.shape[1] + 2))
+    padded_data[1:-1, 1:-1] = data
+    out = np.empty(shape=data.shape)
+    for i in range(1, padded_data.shape[0] - 1):
+        for j in range(1, padded_data.shape[1] - 1):
+            out[i - 1, j - 1] = (
+                first_occupied(padded_data[i + 1 :, j])
+                + first_occupied(padded_data[i, j + 1 :])
+                + first_occupied(padded_data[i - 1 :: -1, j])
+                + first_occupied(padded_data[i, j - 1 :: -1])
+                + first_occupied(np.diag(padded_data[i + 1 :, j + 1 :]))
+                + first_occupied(np.diag(padded_data[i - 1 :: -1, j - 1 :: -1]))
+                + first_occupied(np.diag(padded_data[i - 1 :: -1, j + 1 :]))
+                + first_occupied(np.diag(padded_data[i + 1 :, j - 1 :: -1]))
+            )
+    return out
+
+
+@njit
 def day11_part2(data: np.ndarray) -> int:
     while True:
         neighbors = neighborhood_part2(data)
